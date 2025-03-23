66631fa45106457358f04adab4b7b4c802207af0
youlchikk <yulyamachonina@gmail.com>
2025-03-23 13:46:25 +0300

    HelloApplication

--- client/src/main/java/com/example/client/HelloApplication.java
+++ client/src/main/java/com/example/client/HelloApplication.java
@@ -1,37 +1,21 @@
 package com.example.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import java.net.URL;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        stage.setTitle("BROKER");
+    public void start(Stage stage) throws Exception {
+        Parent root =  (Parent) FXMLLoader.load(getClass().getResource("/com/example/client/hello-view.fxml"));
+        HelloController.setupStage(stage);
 
-
-        Label label = new Label("Hello");
-        label.setLayoutY(80);    // установка положения надписи по оси Y
-        label.setLayoutX(80);   // установка положения надписи по оси X
-
-        Pane group = new Pane(label);
-       // group.getStyleClass().add("root");
-        Scene scene = new Scene(group);
+        Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/styles/helloApplicationCSS.css").toExternalForm());
-
-        //group.setStyle("-fx-background-color: red;");
-
         stage.setScene(scene);
-
-        stage.setWidth(900);
-        stage.setHeight(600);
         stage.show();
-
     }
 
     public static void main(String[] args) {
--- client/src/main/java/com/example/client/HelloController.java
+++ client/src/main/java/com/example/client/HelloController.java
@@ -1,14 +1,53 @@
 package com.example.client;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class HelloController {
-   // @FXML
-   // private Label welcomeText;
+    @FXML
+    private Button buttonLogUser;
+    @FXML
+    private Button buttonRegister;
+    @FXML
+    private Button buttonLogAdmin;
+    @FXML
+    private ImageView imageView1;
+    @FXML
+    private ImageView imageView2;
+    @FXML
+    private ImageView imageView3;
 
-   // @FXML
-   // protected void onHelloButtonClick() {
-       // welcomeText.setText("Welcome to JavaFX Application!");
-   // }
+    @FXML
+    public void initialize() {
+        imageView1.setImage(new Image(getClass().getResource("/images/01.png").toExternalForm()));
+        imageView2.setImage(new Image(getClass().getResource("/images/02.png").toExternalForm()));
+        imageView3.setImage(new Image(getClass().getResource("/images/03.png").toExternalForm()));
+    }
+
+    public static void setupStage(Stage stage) {
+        stage.setTitle("BROKER");
+        stage.setWidth(900);
+        stage.setHeight(700);
+    }
+
+    @FXML
+    private void handleLogUser() {
+        System.out.println("Кнопка 'Войти' нажата!");
+        // Добавьте нужную логику
+    }
+
+    @FXML
+    private void handleRegister() {
+        System.out.println("Кнопка 'Зарегистрироваться' нажата!");
+        // Добавьте нужную логику
+    }
+
+    @FXML
+    private void handleLogAdmin() {
+        System.out.println("Кнопка 'Войти администратором' нажата!");
+        // Добавьте нужную логику
+    }
 }
